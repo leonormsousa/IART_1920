f526803515c665109a6bdba6eb3677fd7bf87070
leonormsousa <leonormsousa@sapo.pt>
2020-03-18 12:19:54 +0000

    Added solved? funtion + class Move + class Node + class SearchTree + function run_level + some changes to class level

--- game.cpp
+++ game.cpp
@@ -13,24 +13,45 @@ const string empty_color = "\33[0;30;40m";
 const string grey_color = "\33[0;97;107m";
 const vector<string> colors = {"\33[0;31;41m", "\33[0;32;42m", "\33[0;33;43m", "\33[0;34;44m", "\33[0;35;45m", "\33[0;36;46m", "\33[0;37;47m"};
 
+//--------------------------------------------------------------------------------------------------------//
+//--------------------------------------------  MOVE -----------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------//
+class Move{
+    private:
+        int direction;
+        char group;
+    public:
+        char getGroup(){return group;}
+        int getDirection(){return direction;}
+        void display(){
+            cout << "Direction: " << direction << " | Group: " << group << "\n";
+        }
+};
+
+//--------------------------------------------------------------------------------------------------------//
+//--------------------------------------------  LEVEL ----------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------//
 class Level{
     private:
-        vector< vector<char> > initial_state;
-        vector< vector<char> > board;
         int number;
+        vector< vector<char> > board;
         map<char, pair<int, string> > groups;
         int cost;
+        int find_jmax(char group);
+        int find_jmin(char group);
+        int find_imax(char group);
+        int find_imin(char group);
     public:
         Level(int number, vector< vector<char> > board);
-        int analise_move(char group, int direction);
-        void move(char group, int direction, int limit);
+        bool analise_move(Move move);
+        void make_move(Move move);
+        bool solved();
         void display();
 };
 
 Level::Level(int number, vector< vector<char> > board){
     this->number=number;
     this->board=board;
-    this->initial_state=board;
     this->cost=0;
     int color_counter=0;
     for (int line=0; line<board.size(); line++){
@@ -55,127 +76,147 @@ Level::Level(int number, vector< vector<char> > board){
     }
 };
 
-int Level::analise_move(char group, int direction){
-    switch(direction){
+int Level::find_imax(char group){
+    int imax=-1;
+    for (int line=0; line<board.size(); line++){
+        for (int column=0; column<board[line].size(); column++){
+            if (board[line][column] == group){
+                if (line>imax)
+                    imax=line;
+            }
+        }
+    }
+    return imax;
+}
+
+int Level::find_jmax(char group){
+    int jmax=-1;
+    for (int line=0; line<board.size(); line++){
+        for (int column=0; column<board[line].size(); column++){
+            if (board[line][column] == group){
+                if (column>jmax)
+                    jmax=column;
+            }
+        }
+    }
+    return jmax;
+}
+
+int Level::find_imin(char group){
+    int imin=board.size();
+    for (int line=0; line<board.size(); line++){
+        for (int column=0; column<board[line].size(); column++){
+            if (board[line][column] == group){
+                if (line<imin)
+                    imin=line;
+            }
+        }
+    }
+    return imin;
+}
+
+int Level::find_jmin(char group){
+    int jmin=board[0].size();
+    for (int line=0; line<board.size(); line++){
+        for (int column=0; column<board[line].size(); column++){
+            if (board[line][column] == group){
+                if (column<jmin)
+                    jmin=column;
+            }
+        }
+    }
+    return jmin;
+}
+
+bool Level::analise_move(Move move){
+    switch(move.getDirection()){
         case DOWN:{
             //find imax
-            int imax=-1;
-            for (int line=0; line<board.size(); line++){
-                for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
-                        if (line>imax)
-                            imax=line;
-                    }
-                }
-            }
+            int imax=find_imax(move.getGroup());
             if (imax==-1)
-                return -1;
+                return false;
             //verify if move is possible
             for (int line=0; line<board.size(); line++){
                 for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
+                    if (board[line][column] == move.getGroup()){
                         if (2*imax-line+1>=board.size())
-                            return -1;
+                            return false;
                         if (board[2*imax-line+1][column]!='0')
-                            return -1;
+                            return false;
                     }
                 }
             }
-            return imax;
+            return true;
         }
         case UP:{
             //find imin
-            int imin=board.size();
-            for (int line=0; line<board.size(); line++){
-                for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
-                        if (line<imin)
-                            imin=line;
-                    }
-                }
-            }
+            int imin=find_imin(move.getGroup());
             if (imin==board.size())
-                return -1;
+                return false;
             //verify if move is possible
             for (int line=0; line<board.size(); line++){
                 for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
+                    if (board[line][column] == move.getGroup()){
                         if (2*imin-line-1<0)
-                            return -1;
+                            return false;
                         if (board[2*imin-line-1][column]!='0')
-                            return -1;
+                            return false;
                     }
                 }
             }
-            return imin;
+            return true;
         }
         case RIGHT:{
             //find jmax
-            int jmax=-1;
-            for (int line=0; line<board.size(); line++){
-                for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
-                        if (column>jmax)
-                            jmax=column;
-                    }
-                }
-            }
+            int jmax=find_jmax(move.getGroup());
             if (jmax==-1)
-                return -1;
+                return false;
             //verify if move is possible
             for (int line=0; line<board.size(); line++){
                 for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
+                    if (board[line][column] == move.getGroup()){
                         if (2*jmax-column+1>=board[0].size())
-                            return -1;
+                            return false;
                         if (board[line][2*jmax-column+1]!='0')
-                            return -1;
+                            return false;
                     }
                 }
             }
-            return jmax;
+            return true;
         }
         case LEFT:{
             //find jmin
-            int jmin=board[0].size();
-            for (int line=0; line<board.size(); line++){
-                for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
-                        if (column<jmin)
-                            jmin=column;
-                    }
-                }
-            }
+            int jmin=find_jmin(move.getGroup());
             if (jmin==board[0].size())
-                return -1;
+                return false;
             //verify if move is possible
             for (int line=0; line<board.size(); line++){
                 for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
+                    if (board[line][column] == move.getGroup()){
                         if (2*jmin-column-1>=board[0].size())
-                            return -1;
+                            return false;
                         if (board[line][2*jmin-column-1]!='0')
-                            return -1;
+                            return false;
                     }
                 }
             }
-            return jmin;
+            return true;
         }
         default:
-            return -1;
+            return false;
     } 
 }
 
-void Level::move(char group, int direction, int limit){
-    switch(direction){
+void Level::make_move(Move move){
+    switch(move.getDirection()){
         case DOWN:{
             //find imax
-            int imax=limit;
+            int imax=find_imax(move.getGroup());
             //make move
             for (int line=0; line<board.size(); line++){
                 for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
-                        board[2*imax-line+1][column]=group;
+                    if (board[line][column] == move.getGroup()){
+                        board[2*imax-line+1][column]=move.getGroup();
                     }
                 }
             }
@@ -183,12 +224,12 @@ void Level::move(char group, int direction, int limit){
         }
         case UP:{
             //find imin
-            int imin=limit;
+            int imin=find_imin(move.getGroup());
             //make move
             for (int line=0; line<board.size(); line++){
                 for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
-                        board[2*imin-line-1][column]=group;
+                    if (board[line][column] == move.getGroup()){
+                        board[2*imin-line-1][column]=move.getGroup();
                     }
                 }
             }
@@ -196,12 +237,12 @@ void Level::move(char group, int direction, int limit){
         }
         case RIGHT:{
             //find jmax
-            int jmax=limit;
+            int jmax=find_jmax(move.getGroup());
             //make move
             for (int line=0; line<board.size(); line++){
                 for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
-                        board[line][2*jmax-column+1]=group;
+                    if (board[line][column] == move.getGroup()){
+                        board[line][2*jmax-column+1]=move.getGroup();
                     }
                 }
             }
@@ -209,12 +250,12 @@ void Level::move(char group, int direction, int limit){
         }
         case LEFT:{
             //find jmin
-            int jmin=limit;
+            int jmin=find_jmin(move.getGroup());
             //make move
             for (int line=0; line<board.size(); line++){
                 for (int column=0; column<board[line].size(); column++){
-                    if (board[line][column] == group){
-                        board[line][2*jmin-column-1]=group;
+                    if (board[line][column] == move.getGroup()){
+                        board[line][2*jmin-column-1]=move.getGroup();
                     }
                 }
             }
@@ -222,6 +263,16 @@ void Level::move(char group, int direction, int limit){
         }
     } 
     this->cost++;
+}
+
+bool Level::solved(){
+    for (int line=0; line<board.size(); line++){
+        for (int column=0; column<board[line].size(); column++){
+            if (board[line][column] == '0')
+                return false;
+        }
+    }
+    return true;
 }
 
 void Level::display(){
@@ -240,13 +291,34 @@ void Level::display(){
     }
 }
 
+//--------------------------------------------------------------------------------------------------------//
+//--------------------------------------------  NODE -----------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------//
+class Node{
+    public:
+        Level state;
+        Node* father;
+        Move move;
+        int depth;
+        int cost;
+};
 
+class SearchTree{
+    public:
+        vector<Node> nodes;
+};
+
+//--------------------------------------------------------------------------------------------------------//
+//----------------------------------------  FoldingBlocks ------------------------------------------------//
+//--------------------------------------------------------------------------------------------------------//
 class FoldingBlocks{
     private:
         vector<Level> levels;
     public:
         FoldingBlocks();
-        void print(){ levels[3].display();}
+        void print(int level){ levels[level].display();}
+        void run_level(int level);
+        vector<Move> solve(Level level);
 };
 
 FoldingBlocks::FoldingBlocks(){
@@ -264,7 +336,27 @@ FoldingBlocks::FoldingBlocks(){
     levels.push_back(Level(6, level6));
 }
 
+vector<Move> FoldingBlocks::solve(Level level){
+    /////////////////////////////THIS>////////////////////////777777777777////////////7777777777777777777777777/////////////////////////////////////777
+}
+
+void FoldingBlocks::run_level(int level){
+    Level current_level = levels[level];
+    vector<Move> moves = solve(current_level);
+    current_level.display();
+    if (moves.empty())
+        cout << "No solution is possible for level " << level << '\n';
+    else{
+        for (int i=0; i<moves.size(); i++){
+                current_level.make_move(moves[i]);
+                moves[i].display();
+                current_level.display();
+        }
+        cout << "Level Solved with success.\nFinal Cost: " << moves.size() << "\n"; 
+    }
+}
+
 int main(){
     FoldingBlocks foldingBlocks;
-    foldingBlocks.print();
+    foldingBlocks.print(1);
 }
