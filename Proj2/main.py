f017f849767c1ccd989cbacd6611db2b124423bc
jfcaraujo <jfcaraujo99@gmail.com>
2020-05-15 11:59:51 +0100

    Updated independant data

--- Proj2/main.py
+++ Proj2/main.py
@@ -21,7 +21,7 @@ start_date=datetime.strptime(min(data.iloc[:,2]),"%Y-%m-%d")
 independent_data=data.iloc[:,0:1].values
 dates=data.iloc[:,2].values
 new_dates=[]
-dependant_data=data.iloc[:,4:].values
+dependant_data=data.iloc[:,9:].values
 
 for row in data.iloc[:,2].values: #calculates diff of dates in days
   new_dates.append((datetime.strptime(row,"%Y-%m-%d")-start_date).days)   
@@ -29,4 +29,5 @@ for row in data.iloc[:,2].values: #calculates diff of dates in days
 # independent_data[:,0] = labelencoder_X.fit_transform(independent_data[:,0])
 independent_data = OneHotEncoder().fit_transform(independent_data).toarray()
 independent_data = np.append(independent_data,np.array(new_dates)[:, None],axis=1) #append dates
+independent_data = np.append(independent_data,data.iloc[:,3:9].values,axis=1)
 print(independent_data)
