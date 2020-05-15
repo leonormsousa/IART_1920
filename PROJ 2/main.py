8335ed3e27a08113db022bdfa50619c9e2b2cbe0
jfcaraujo <jfcaraujo99@gmail.com>
2020-05-15 02:57:58 +0100

    Preprocessed data

--- PROJ 2/main.py
+++ PROJ 2/main.py
@@ -7,15 +7,26 @@ This is a temporary script file.
 
 # Demo file for Spyder Tutorial
 # Hans Fangohr, University of Southampton, UK
-import csv
-
-def hello():
-    """Print "Hello World" and return None."""
-    print("Hello World")
+import pandas as pd
+from datetime import datetime
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import numpy as np
 
 # Main program starts here
-hello()
-with open('data.csv', newline='') as csvfile:
-   spamreader = csv.reader(csvfile, delimiter=',')
-   for row in spamreader:
-       print('|'.join(row))
+
+data = pd.read_csv('data.csv')
+labelencoder_X = LabelEncoder()
+
+start_date=datetime.strptime(min(data.iloc[:,2]),"%Y-%m-%d")
+independent_data=data.iloc[:,0:1].values
+dates=data.iloc[:,2].values
+new_dates=[]
+dependant_data=data.iloc[:,4:].values
+
+for row in data.iloc[:,2].values: #calculates diff of dates in days
+  new_dates.append((datetime.strptime(row,"%Y-%m-%d")-start_date).days)   
+  
+# independent_data[:,0] = labelencoder_X.fit_transform(independent_data[:,0])
+independent_data = OneHotEncoder().fit_transform(independent_data).toarray()
+independent_data = np.append(independent_data,np.array(new_dates)[:, None],axis=1) #append dates
+print(independent_data)
